9a91ad3633b823227deed80f2d57851b24745c9e
Felipe <felipepirovano95@gmail.com>
2021-11-09 11:43:05 -0300

    fundamentos react - listando repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import { useState, useEffect } from "react";
 
-//https://api.github.com/users/Felipepirova/repos
-
-const repository = {
-  name:'unform',
-  description:'Forms in React',
-  link:'https://github.com/Felipepirova/fundamentosignite/commits/main'
-}
-
 export function RepositoryList(){
 
   const [repositories, setRepositories] = useState([])
@@ -16,15 +8,14 @@ export function RepositoryList(){
   useEffect(()=>{
     fetch('https://api.github.com/users/Felipepirova/repos')
     .then(response => response.json()
-    .then(data => console.log(data)))
+    .then(data => setRepositories(data)))
   },[])
 
   return(
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository =><RepositoryItem key={repository.name} repository={repository}/>)}
       </ul>
     </section>
   )
